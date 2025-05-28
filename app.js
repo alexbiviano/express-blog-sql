50b6236eb33b817903260a378dcc573333d9540d
alexbiviano <bivianoalex63@gmail.com>
2025-05-28 15:44:59 +0200

    [upd] server listen port 3000

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express');  
+const dbConnection = require('./dbConfig'); 
 
+const app = express();  
+app.use(express.json());  
 
-const postRouter = require('./routers/post');
+ 
+app.delete('/posts/:id', (req, res) => {  
+  const postId = req.params.id;  
+  const query = 'DELETE FROM posts WHERE id = ?'; // Sostituisci 'posts' con il nome della tua tabella  
 
+  dbConnection.query(query, [postId], (err, results) => {  
+    if (err) {  
+      console.error('Errore nella query: ', err);  
+      return res.status(500).json({ error: 'Errore nell\'eliminazione del post' });  
+    }  
+    
+    if (results.affectedRows === 0) {  
+      return res.status(404).json({ error: 'Post non trovato' });  
+    }  
+    
+    res.status(204).send();  
+  });  
+});  
 
-app.use(express.json());
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-    console.log('Benvenuto nel mio Blog');
-    res.send('Benvenuto nel mio Blog')
-})
-
-app.use('/posts', postRouter);
-
-//listen
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+ 
+const PORT = process.env.PORT || 3000;  
+app.listen(PORT, () => {  
+  console.log(`Server in ascolto sulla porta ${PORT}`);  
+});  
